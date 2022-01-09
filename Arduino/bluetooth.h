71f57f4ba860d3c00ab45730d804f496eef1fc49
Jench2103 <jench2103@gmail.com>
2022-01-10 03:39:18 +0800

    add double2str() to turn GPS coordinates to strings with fixed length

--- Arduino/bluetooth.h
+++ Arduino/bluetooth.h
@@ -2,27 +2,55 @@
 #define BLUETOOTH_H
 #include "Arduino.h"
 #include <SoftwareSerial.h>
+#include <math.h>
+
+String double2str(double num, int length)
+{
+    String result = "";
+    bool positive = num > 0.0;
+
+    if (num == 0.0) {
+        result = "0.";
+
+        for (int i = 0; i < length - 2; ++i) {
+            result += '0';
+        }
+        
+        return result;
+    }
+
+    for (int i = length - 1; i >= 0; --i)
+    {
+        if (positive ? num > pow(10, i) : num * (-1) > pow(10, i))
+        {
+            result = String(num, positive ? length - (i + 2) : length - (i + 3));
+            break;
+        }
+    }
+
+    return result;
+}
 
 class BLUETOOTH
 {
 public:
     BLUETOOTH();
-    int n_after_point_lo = 5;                                                   // gps_longtitude傳送時小數點後的位數
-    int n_after_point_la = 5;                                                   // gps_latitude傳送時小數點後的位數
+    int n_after_point_lo = 5; // gps_longtitude傳送時小數點後的位數
+    int n_after_point_la = 5; // gps_latitude傳送時小數點後的位數
 
-    // 接收手機藍芽傳的訊號，回傳值: if_buzzer_on 
+    // 接收手機藍芽傳的訊號，回傳值: if_buzzer_on
     // 收到'0': 不開啟蜂鳴器
     // 收到'a': 傳使用者資訊給手機，開啟蜂鳴器
-    int phone_to_arduino(bool event, double gps_longtitude = 0, double gps_latitude = 0, int bpm = 0); 
-    int if_buzzer_on = -1;  // 未收到指示: -1；不要開啟蜂鳴器: 0；開啟蜂鳴器: 1
-    
+    int phone_to_arduino(bool event, double gps_longtitude = 0, double gps_latitude = 0, int bpm = 0);
+    int if_buzzer_on = -1; // 未收到指示: -1；不要開啟蜂鳴器: 0；開啟蜂鳴器: 1
+
     void send_1_to_phone(); // 藍芽傳'1'給手機
 
     void reset(); // 事件結束，將if_buzzer_on回復預設值-1
 
 private:
     SoftwareSerial BTSerial;
-    
+
     void arduino_to_phone(double gps_longtitude, double gps_latitude, int bpm); // 事件發生時傳送地點、心律等資訊至手機，傳送格式: ，傳送格式: "&heart_rate=76&gps_longitude=121.7654&gps_latitude=23.84723"
 };
 
@@ -37,22 +65,21 @@ int BLUETOOTH::phone_to_arduino(bool event, double gps_longtitude = 0, double gp
 {
     if (BTSerial.available())
     {
-        
+
         char message_from_phone = BTSerial.read();
 
-        switch(message_from_phone)
-        {            
-            //收到'0': 不開啟蜂鳴器
-            case '0':
-                if_buzzer_on = 0;
-                break;
-            //收到'a': 傳使用者資訊給手機，開啟蜂鳴器
-            case 'a':
-                if_buzzer_on = 1;
-                arduino_to_phone(gps_longtitude, gps_latitude, bpm);
-                break;
-            default:
-                break;
+        switch (message_from_phone)
+        {
+        case '0': //收到'0': 不開啟蜂鳴器
+            if_buzzer_on = 0;
+            break;
+
+        case 'a': //收到'a': 傳使用者資訊給手機，開啟蜂鳴器
+            if_buzzer_on = 1;
+            arduino_to_phone(gps_longtitude, gps_latitude, bpm);
+            break;
+        default:
+            break;
         }
     }
 
@@ -72,7 +99,9 @@ void BLUETOOTH::send_1_to_phone() // 藍芽傳'1'給手機
 void BLUETOOTH::arduino_to_phone(double gps_longtitude, double gps_latitude, int bpm) // 事件發生時傳送地點、心律等資訊至手機，傳送格式: &heart_rate=76&gps_longitude=121.7654&gps_latitude=23.84723
 {
     String string_to_phone;
-    string_to_phone = "&heart_rate=" + (String(bpm).length() == 3? String(bpm) : '0' + String(bpm)) + "&gps_longitude=" + String(gps_longtitude, n_after_point_lo) + "&gps_latitude=" + String(gps_latitude, n_after_point_la);
+
+    // string_to_phone = "&heart_rate=" + (String(bpm).length() == 3 ? String(bpm) : '0' + String(bpm)) + "&gps_longitude=" + String(gps_longtitude, n_after_point_lo) + "&gps_latitude=" + String(gps_latitude, n_after_point_la);
+    string_to_phone = "&heart_rate=" + (String(bpm).length() == 3 ? String(bpm) : '0' + String(bpm)) + "&gps_longitude=" + double2str(gps_longtitude, 9) + "&gps_latitude=" + double2str(gps_latitude, 8);
 
     BTSerial.print(string_to_phone);
 }
